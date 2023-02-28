9f142f48482ca5c8ab51a8bf9a051cf6f31a6359
rsaldana <ruben.saldana-ruiz@capgemini.com>
2023-02-28 12:25:20 +0100

    Characters list

--- src/app/shared/components/character-card/character-card.component.ts
+++ src/app/shared/components/character-card/character-card.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CharacterModel } from '../../../core/models/character.model';
 
 @Component({
   selector: 'app-character-card',
   templateUrl: './character-card.component.html',
-  styleUrls: ['./character-card.component.scss']
+  styleUrls: ['./character-card.component.scss'],
 })
-export class CharacterCardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class CharacterCardComponent {
+  @Input() character: CharacterModel = {
+    id: 0,
+    name: '',
+    image: '',
+    species: '',
+    gender: '',
+    created: '',
+    status: '',
+  };
 }
