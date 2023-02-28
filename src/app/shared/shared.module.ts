00aeb217bc5ada9690cd71bfbd73a2e53c1b7095
rsaldana <ruben.saldana-ruiz@capgemini.com>
2023-02-28 09:35:41 +0100

    First components

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RouterModule],
+  exports: [],
+})
+export class SharedModule {}
