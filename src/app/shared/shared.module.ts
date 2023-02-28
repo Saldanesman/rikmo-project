19c5be0d5dd596a8e8ef2a68595e3d9cdc7deb73
rsaldana <ruben.saldana-ruiz@capgemini.com>
2023-02-28 10:20:38 +0100

    Navbar - First version

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { CharacterCardComponent } from './components/character-card/character-card.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavBarComponent, CharacterCardComponent],
   imports: [CommonModule, RouterModule],
-  exports: [],
+  exports: [NavBarComponent, CharacterCardComponent],
 })
 export class SharedModule {}
